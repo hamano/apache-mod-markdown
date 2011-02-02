7bc2c518e69106e0f91a30a54d7d76ac837e6f03
Tsukasa Hamano <code@cuspy.org>
2011-02-03 02:10:09 +0900

    indent -kr -ts255

--- mod_markdown.c
+++ mod_markdown.c
@@ -47,147 +47,146 @@
 
 module AP_MODULE_DECLARE_DATA markdown_module;
 
-typedef struct{
+typedef struct {
     const char *css;
-}markdown_conf;
+} markdown_conf;
 
 extern char *mkd_doc_title(Document *);
 
 #define P(s) ap_rputs(s, r)
 
-void markdown_output(Document *doc, request_rec *r)
+void markdown_output(Document * doc, request_rec * r)
 {
-	char *title;
-	int ret;
-	int size;
-	char *p;
-	markdown_conf *conf;
+    char *title;
+    int ret;
+    int size;
+    char *p;
+    markdown_conf *conf;
 
-	conf = (markdown_conf *)ap_get_module_config(r->per_dir_config,
-												 &markdown_module);
-	ret = mkd_compile(doc, 0);
-	ap_rputs("<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n", r);
-	ap_rputs(
-		"<!DOCTYPE html PUBLIC \n"
-		"          \"-//W3C//DTD XHTML 1.0 Strict//EN\"\n"
-		"          \"http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd\">\n",
-		r);
-	ap_rputs("<html xmlns=\"http://www.w3.org/1999/xhtml\">\n", r);
-	ap_rputs("<head>\n", r);
+    conf = (markdown_conf *) ap_get_module_config(r->per_dir_config,
+                                                  &markdown_module);
+    ret = mkd_compile(doc, 0);
+    ap_rputs("<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n", r);
+    ap_rputs("<!DOCTYPE html PUBLIC \n"
+             "          \"-//W3C//DTD XHTML 1.0 Strict//EN\"\n"
+             "          \"http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd\">\n",
+             r);
+    ap_rputs("<html xmlns=\"http://www.w3.org/1999/xhtml\">\n", r);
+    ap_rputs("<head>\n", r);
 
-	if(conf->css){
-		ap_rputs("<meta http-equiv=\"Content-Type\""
-				 " content=\"text/html; charset=UTF-8\" />\n", r);
-		ap_rputs("<meta http-equiv=\"Content-Style-Type\""
-				 " content=\"text/css\" />\n", r);
-		ap_rprintf(r, "<link rel=\"stylesheet\" href=\"%s\" />\n", conf->css);
-	}
-	if(title = mkd_doc_title(doc)){
-		ap_rprintf(r, "<title>%s</title>\n", title);
-	}
-	ap_rputs("</head>\n", r);
-	ap_rputs("<body>\n", r);
-	if(title){
-		ap_rprintf(r, "<h1 class=\"title\">%s</h1>\n", title);
-	}
-	if((size = mkd_document(doc, &p)) != EOF){
-		ap_rwrite(p, size, r);
-	}
-	ap_rputc('\n', r);
-	ap_rputs("</body>\n", r);
-	mkd_cleanup(doc);
+    if (conf->css) {
+        ap_rputs("<meta http-equiv=\"Content-Type\""
+                 " content=\"text/html; charset=UTF-8\" />\n", r);
+        ap_rputs("<meta http-equiv=\"Content-Style-Type\""
+                 " content=\"text/css\" />\n", r);
+        ap_rprintf(r, "<link rel=\"stylesheet\" href=\"%s\" />\n",
+                   conf->css);
+    }
+    if (title = mkd_doc_title(doc)) {
+        ap_rprintf(r, "<title>%s</title>\n", title);
+    }
+    ap_rputs("</head>\n", r);
+    ap_rputs("<body>\n", r);
+    if (title) {
+        ap_rprintf(r, "<h1 class=\"title\">%s</h1>\n", title);
+    }
+    if ((size = mkd_document(doc, &p)) != EOF) {
+        ap_rwrite(p, size, r);
+    }
+    ap_rputc('\n', r);
+    ap_rputs("</body>\n", r);
+    mkd_cleanup(doc);
 }
 
-void raw_output(FILE *fp, request_rec *r)
+void raw_output(FILE * fp, request_rec * r)
 {
-	char buf[1024];
-	size_t len;
-	while(1){
-		len = fread(buf, 1, 1024, fp);
-		if(len <= 0){
-			break;
-		}
-		ap_rwrite(buf, len, r);
-	}
+    char buf[1024];
+    size_t len;
+    while (1) {
+        len = fread(buf, 1, 1024, fp);
+        if (len <= 0) {
+            break;
+        }
+        ap_rwrite(buf, len, r);
+    }
 }
 
 /* The markdown handler */
-static int markdown_handler(request_rec *r)
+static int markdown_handler(request_rec * r)
 {
-	FILE *fp;
-	Document *doc;
-	int ret;
+    FILE *fp;
+    Document *doc;
+    int ret;
 
-    if(strcmp(r->handler, "markdown")){
+    if (strcmp(r->handler, "markdown")) {
         return DECLINED;
     }
 
-    if (r->header_only){
-		return OK;
-	}
+    if (r->header_only) {
+        return OK;
+    }
 
-	ap_log_rerror(APLOG_MARK, APLOG_DEBUG, 0, r,
-				  "markdown_handler(): %s", r->filename);
+    ap_log_rerror(APLOG_MARK, APLOG_DEBUG, 0, r,
+                  "markdown_handler(): %s", r->filename);
 
-	fp = fopen(r->filename, "r");
-	if(fp == NULL){
-		switch(errno){
-		case ENOENT:
-			return HTTP_NOT_FOUND;
-		case EACCES:
-			return HTTP_FORBIDDEN;
-		default:
-			ap_log_rerror(APLOG_MARK, APLOG_ERR, 0, r,
-						  "open error, errno: %d\n", errno);
-			return HTTP_INTERNAL_SERVER_ERROR;
-		}
-	}
+    fp = fopen(r->filename, "r");
+    if (fp == NULL) {
+        switch (errno) {
+        case ENOENT:
+            return HTTP_NOT_FOUND;
+        case EACCES:
+            return HTTP_FORBIDDEN;
+        default:
+            ap_log_rerror(APLOG_MARK, APLOG_ERR, 0, r,
+                          "open error, errno: %d\n", errno);
+            return HTTP_INTERNAL_SERVER_ERROR;
+        }
+    }
 
-	if(r->args && !strcasecmp(r->args, "raw")){
-		r->content_type = "text/plain";
-		raw_output(fp, r);
-		fclose(fp);
-	}else{
-		r->content_type = "text/html";
-		doc = mkd_in(fp, 0);
-		fclose(fp);
-		if(doc == NULL){
-			ap_log_rerror(APLOG_MARK, APLOG_ERR, 0, r,
-						  "mkd_in() returned NULL\n");
-			return HTTP_INTERNAL_SERVER_ERROR;
-		}
-		markdown_output(doc, r);
-	}
+    if (r->args && !strcasecmp(r->args, "raw")) {
+        r->content_type = "text/plain";
+        raw_output(fp, r);
+        fclose(fp);
+    } else {
+        r->content_type = "text/html";
+        doc = mkd_in(fp, 0);
+        fclose(fp);
+        if (doc == NULL) {
+            ap_log_rerror(APLOG_MARK, APLOG_ERR, 0, r,
+                          "mkd_in() returned NULL\n");
+            return HTTP_INTERNAL_SERVER_ERROR;
+        }
+        markdown_output(doc, r);
+    }
 
     return OK;
 }
 
 
 
-static void *markdown_config(apr_pool_t *p, char *dummy)
+static void *markdown_config(apr_pool_t * p, char *dummy)
 {
-    markdown_conf *c = (markdown_conf *)apr_pcalloc(p, sizeof(markdown_conf));
+    markdown_conf *c =
+        (markdown_conf *) apr_pcalloc(p, sizeof(markdown_conf));
     memset(c, 0, sizeof(markdown_conf));
-    return (void *)c;
+    return (void *) c;
 }
 
-static const char *set_markdown_css(cmd_parms *cmd,
-									void *conf,
-									const char *arg)
+static const char *set_markdown_css(cmd_parms * cmd, void *conf,
+                                    const char *arg)
 {
-    markdown_conf *c = (markdown_conf*)conf;
+    markdown_conf *c = (markdown_conf *) conf;
     c->css = arg;
     return NULL;
 }
 
-static const command_rec markdown_cmds[] =
-{
+static const command_rec markdown_cmds[] = {
     AP_INIT_TAKE1("MarkdownCSS", set_markdown_css, NULL, OR_ALL,
-				  "set CSS"),
+                  "set CSS"),
     {NULL}
 };
 
-static void markdown_register_hooks(apr_pool_t *p)
+static void markdown_register_hooks(apr_pool_t * p)
 {
     ap_hook_handler(markdown_handler, NULL, NULL, APR_HOOK_MIDDLE);
 }
@@ -195,10 +194,10 @@ static void markdown_register_hooks(apr_pool_t *p)
 /* Dispatch list for API hooks */
 module AP_MODULE_DECLARE_DATA markdown_module = {
     STANDARD20_MODULE_STUFF,
-    markdown_config,        /* create per-dir    config structures */
-    NULL,                   /* merge  per-dir    config structures */
-    NULL,                   /* create per-server config structures */
-    NULL,                   /* merge  per-server config structures */
-    markdown_cmds,          /* table of config file commands       */
-    markdown_register_hooks /* register hooks                      */
+    markdown_config,            /* create per-dir    config structures */
+    NULL,                       /* merge  per-dir    config structures */
+    NULL,                       /* create per-server config structures */
+    NULL,                       /* merge  per-server config structures */
+    markdown_cmds,              /* table of config file commands       */
+    markdown_register_hooks     /* register hooks                      */
 };
